2f5e5879faef09db915d1a0c1edaf893f7b49105
hyemdev <devdev.hyem@gmail.com>
2024-01-05 17:20:32 +0900

    update : 사이드메뉴 오류수정

--- src/components/AsideAdm.js
+++ src/components/AsideAdm.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SideMenuWrap } from "../styles/AsideStyle";
 import Sider from "antd/es/layout/Sider";
 import { Menu } from "antd";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowRightFromBracket,
@@ -12,6 +12,17 @@ import { postLogout } from "../api/client";
 
 const AsideAdm = () => {
   const navigate = useNavigate();
+
+  const location = useLocation();
+  const [defaultSelectedKeys, setDefaultSelectedKeys] = useState(
+    location.pathname,
+  );
+  console.log("location", location);
+
+  useEffect(() => {
+    setDefaultSelectedKeys(location.pathname);
+  }, [location]);
+
   function getItem(label, key, icon, children) {
     return {
       key,
@@ -22,16 +33,16 @@ const AsideAdm = () => {
   }
   const menuItems2 = [
     // getItem("홈", "1", null),
-    getItem(<Link to="/home">홈</Link>, "1", null),
+    getItem(<Link to="/home">홈</Link>, "/home", null),
     getItem("과정관리", "sub1", null, [
-      getItem(<Link to="/class">과정등록 • 관리</Link>, "3"),
-      getItem(<Link to="/jobmanager">취업 담당자 관리</Link>, "4"),
+      getItem(<Link to="/class">과정등록 • 관리</Link>, "/class"),
+      getItem(<Link to="/jobmanager">취업 담당자 관리</Link>, "/jobmanager"),
     ]),
     getItem("수강생 관리", "sub2", null, [
-      getItem(<Link to="/student">수강생 등록 • 관리</Link>, "7"),
-      getItem(<Link to="/portfolio">포트폴리오 관리</Link>, "8"),
+      getItem(<Link to="/student">수강생 등록 • 관리</Link>, "/student"),
+      getItem(<Link to="/portfolio">포트폴리오 관리</Link>, "/portfolio"),
     ]),
-    getItem(<Link to="/company">기업등록 • 관리</Link>, "9", null),
+    getItem(<Link to="/company">기업등록 • 관리</Link>, "/company", null),
   ];
 
   const handleLogout = () => {
@@ -61,8 +72,8 @@ const AsideAdm = () => {
             width: 200,
             background: "#ffffff",
           }}
-          defaultSelectedKeys={["1"]}
-          // defaultOpenKeys={["sub1"]}
+          defaultOpenKeys={["sub1", "sub2"]}
+          defaultSelectedKeys={[defaultSelectedKeys]}
           mode={"inline"}
           theme={"light"}
           items={menuItems2}
